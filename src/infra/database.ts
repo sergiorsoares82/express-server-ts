eac94995bb8d201f60a71064972b98ede60761e5
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-05-01 17:17:55 -0300

    test: logs environment variables to debug

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,6 +1,14 @@
 import { Client } from 'pg';
 
 const query = async (queryObject: any) => {
+  console.log('Environment variables:', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: parseInt(process.env.POSTGRES_PORT),
